58c5ba44dc98c6dd35f4fc30da4e61606102479e
errmzt <133062833+errmzt@users.noreply.github.com>
2026-01-22 14:43:36 +0100

    Update HotkeyManager.cs

--- HotkeyManager.cs
+++ HotkeyManager.cs
@@ -34,3 +34,72 @@ public class HotkeyManager
         }
     }
 }
+using System;
+using System.Runtime.InteropServices;
+using System.Windows.Input;
+using System.Windows.Interop;
+
+namespace CustomSearchApp.Services
+{
+    public class HotkeyManager : IDisposable
+    {
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private const int HOTKEY_ID = 9000;
+        private const uint MOD_ALT = 0x0001;
+        private const uint VK_SPACE = 0x20;
+
+        private IntPtr _windowHandle;
+        private HwndSource _source;
+        private Action _hotkeyAction;
+
+        public void Register(IntPtr windowHandle, Action hotkeyAction)
+        {
+            _windowHandle = windowHandle;
+            _hotkeyAction = hotkeyAction;
+            
+            // Rejestracja źródła wiadomości okna
+            _source = HwndSource.FromHwnd(windowHandle);
+            _source.AddHook(HwndHook);
+
+            // Rejestracja hotkeya
+            if (!RegisterHotKey(windowHandle, HOTKEY_ID, MOD_ALT, VK_SPACE))
+            {
+                throw new InvalidOperationException("Nie można zarejestrować skrótu klawiszowego.");
+            }
+        }
+
+        private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            const int WM_HOTKEY = 0x0312;
+            
+            if (msg == WM_HOTKEY && wParam.ToInt32() == HOTKEY_ID)
+            {
+                _hotkeyAction?.Invoke();
+                handled = true;
+            }
+            
+            return IntPtr.Zero;
+        }
+
+        public void Dispose()
+        {
+            if (_source != null)
+            {
+                _source.RemoveHook(HwndHook);
+                _source.Dispose();
+                _source = null;
+            }
+            
+            if (_windowHandle != IntPtr.Zero)
+            {
+                UnregisterHotKey(_windowHandle, HOTKEY_ID);
+                _windowHandle = IntPtr.Zero;
+            }
+        }
+    }
+}
