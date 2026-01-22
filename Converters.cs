b283138a0d161e0c624deea8d441e5eb3e8a1b0c
errmzt <133062833+errmzt@users.noreply.github.com>
2026-01-22 12:59:12 +0100

    Create Converters.cs

---
+++ Converters.cs
@@ -0,0 +1,13 @@
+// Dodaj do App.xaml.cs lub nowy plik Converters.cs
+public class BoolToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (value is bool && (bool)value) ? Visibility.Visible : Visibility.Collapsed;
+    }
+    
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value is Visibility visibility && visibility == Visibility.Visible;
+    }
+}
