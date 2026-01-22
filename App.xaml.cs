93ab3a5a4e9f2821f383e0720e3ee6fbeb2c6fab
errmzt <133062833+errmzt@users.noreply.github.com>
2026-01-22 14:47:25 +0100

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -18,3 +18,59 @@ namespace CustomSearchApp
         }
     }
 }
+using CustomSearchApp.Services;
+using CustomSearchApp.ViewModels;
+using System;
+using System.Windows;
+
+namespace CustomSearchApp
+{
+    public partial class App : Application
+    {
+        private HotkeyManager _hotkeyManager;
+        private MainWindow _mainWindow;
+        
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            
+            // Utwórz i pokaż główne okno
+            _mainWindow = new MainWindow();
+            _mainWindow.DataContext = new MainViewModel();
+            _mainWindow.Hide(); // Ukryj na starcie
+            
+            // Zarejestruj globalny hotkey
+            _hotkeyManager = new HotkeyManager();
+            _hotkeyManager.Register(_mainWindow.GetWindowHandle(), ToggleMainWindow);
+            
+            // Ustaw zamknięcie hotkeya przy zamykaniu aplikacji
+            _mainWindow.Closed += (s, args) => _hotkeyManager.Dispose();
+        }
+        
+        private void ToggleMainWindow()
+        {
+            if (_mainWindow.IsVisible)
+            {
+                _mainWindow.Hide();
+            }
+            else
+            {
+                _mainWindow.Show();
+                _mainWindow.Activate();
+                _mainWindow.SearchTextBox.Focus();
+            }
+        }
+    }
+}
+
+// Extension method dla Window do pobrania handle
+public static class WindowExtensions
+{
+    [System.Runtime.InteropServices.DllImport("user32.dll")]
+    private static extern IntPtr GetWindowLongPtr(IntPtr hWnd, int nIndex);
+    
+    public static IntPtr GetWindowHandle(this Window window)
+    {
+        return new System.Windows.Interop.WindowInteropHelper(window).Handle;
+    }
+}
