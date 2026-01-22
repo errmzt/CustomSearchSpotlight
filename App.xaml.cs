19d2a853bcbf09191cc3752d866df57e98c7fca4
errmzt <133062833+errmzt@users.noreply.github.com>
2026-01-22 05:19:58 +0100

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,44 +1,20 @@
-using System;
 using System.Windows;
-using Microsoft.Extensions.DependencyInjection;
-using CustomSearchSpotlight.Services;
-using CustomSearchSpotlight.ViewModels;
 
-namespace CustomSearchSpotlight
+namespace CustomSearchApp
 {
     public partial class App : Application
     {
-        private IServiceProvider _serviceProvider;
-
-        public App()
-        {
-            var services = new ServiceCollection();
-            ConfigureServices(services);
-            _serviceProvider = services.BuildServiceProvider();
-        }
-
-        private void ConfigureServices(IServiceCollection services)
-        {
-            // View Models
-            services.AddSingleton<MainViewModel>();
-
-            // Services
-            services.AddSingleton<IAIService, GeminiService>();
-            services.AddSingleton<ISearchService, WindowsSearchService>();
-            services.AddSingleton<IVoiceService, WindowsVoiceService>();
-            services.AddSingleton<HotkeyManager>();
-            services.AddSingleton<SettingsManager>();
-
-            // Main Window
-            services.AddSingleton<MainWindow>();
-        }
-
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-
-            var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
-            mainWindow.Show();
+            
+            // Możesz dodać globalne exception handling tutaj
+            DispatcherUnhandledException += (sender, args) =>
+            {
+                MessageBox.Show($"Wystąpił nieoczekiwany błąd:\n{args.Exception.Message}", 
+                    "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                args.Handled = true;
+            };
         }
     }
 }
